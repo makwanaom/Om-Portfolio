07c16c15fe5c20179ef6cbf3d7ba0f84dd2c742b
makwanaom <117265350+makwanaom@users.noreply.github.com>
2023-04-29 10:02:50 +0530

    solved 404 error

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           <Homeee open ={openSidebar} onClose ={() => setopenSidebar(false)}/>
 
       </div>
-      <Routes>
+      <Routes basename="/tothepoint_login">
       <Route path="/" element={<Layout />} >
       <Route index element={<Home/>}/>
       
